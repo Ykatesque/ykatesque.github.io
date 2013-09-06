453a5a885cc44a8607ffbc93640167f79f55f9aa
Yekaterina Novikova <yanoviko@ucdavis.edu>
2013-09-06 02:32:39 -0700

    jquery fix

--- js/script.js
+++ js/script.js
@@ -17,22 +17,22 @@ preload([
 
 
 //fade in different texture bgs on Ykat logotype
-var scrollSpeed = 1000;
+var scrollSpeed = 2000;
 var currentImage = 0;
 var stickyNavTop = $('.nav').offset().top;
 var debounceTimer = null;
 var imageLoopTimer = null;
 
 function nextImage(currentImage) {
-    currentImage = ((currentImage + 1)); // we have images 1..3 to cycle through
+    currentImage = (currentImage + 1);
     if (currentImage == 4) currentImage = 1;
     console.log(currentImage);
-    $('.display-sweet-image-behind').fadeTo(80, 0.7, function () {
-        $(this).css("background-image", "url(" + currentImage + ".png)", "opacity", "1").fadeTo(80, 1);
+    $('.display-sweet-image-behind').fadeTo(90, 0.65, function () {
+        $(this).css("background-image", "url(" + currentImage + ".png)", "opacity", "0.95").fadeTo(90, 1.0);
     });
 
     return currentImage;
-    console.log(currentImage);
+
 }
 
 var stickyNav = function () {
